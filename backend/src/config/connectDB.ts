c20e9fc8e72b84776355946a97c61dd4bbe874b7
JohanVillard <styx.locke@gmail.com>
2025-04-08 17:58:09 +0200

    fix: Ajout des extensions lors des imports de module (Passage en ESM)

--- backend/src/config/connectDB.ts
+++ backend/src/config/connectDB.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg';
-import config from './config';
+import config from './config.js';
 
 const pool = new Pool({
-    user: config.db.user,
-    host: config.db.host,
-    port: config.db.port,
-    password: config.db.password,
-    database: config.db.database,
+	user: config.db.user,
+	host: config.db.host,
+	port: config.db.port,
+	password: config.db.password,
+	database: config.db.database,
 });
 
 export default pool;
--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -1,21 +1,21 @@
 import swaggerJSDoc from 'swagger-jsdoc';
-import config from './config';
+import config from './config.js';
 
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API iKanban',
-            version: '1.0.0',
-            description: "Documentation de l'API iKanban",
-        },
-        servers: [
-            {
-                url: `http://localhost:${config.server.port}`,
-            },
-        ],
-    },
-    apis: ['./src/routes/*.ts'],
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'API iKanban',
+			version: '1.0.0',
+			description: "Documentation de l'API iKanban",
+		},
+		servers: [
+			{
+				url: `http://localhost:${config.server.port}`,
+			},
+		],
+	},
+	apis: ['./src/routes/*.ts'],
 };
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
